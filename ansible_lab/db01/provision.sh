9df62f835f1156ef08830cea6b18b1e60d6eb184
adrianohenriquess <adriano.henriquesds@gmail.com>
2022-05-11 12:00:20 -0300

    vagrant e ansible lab

---
+++ ansible_lab/db01/provision.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+cat << EOT >> /home/vagrant/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDPw7A2EVSIOGHWPApKddYwpZf8zl8oyRymsN+6td1nBgZB9wpJoZUykYsVmipHHzPQs/SSjw+8aKANCRQval1+64iw3ACcDIEKM4PYOozrdOwBgg/8J3CDIo4vujCElasXGEJDyXTLt3TOJccuSYu/r2yBIgJ28NKLELb7wsfOb2aiBhYsgpbPt4E2VMx+rHInUaMWYx3IEYykd6SGu3GEG6ZhjfHUoHWt2o1zoFSQdtyY0r+pYhGwSxjqq7wWstkyYeEPaUq8gEnS6ouSmrw3eA+3GtNG1k4Nq8CJiPv0o3RYnJCupO84aaAYKtpR4zkoFg6Of9pIM1KGqbONQWax vagrant@control-node
+EOT
